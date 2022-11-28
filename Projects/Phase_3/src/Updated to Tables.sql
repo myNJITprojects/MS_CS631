11215a0f99b6b3f1be8e163866ccbcb9698c0934
Andres <jairo.perez@japc.me>
2022-11-27 22:02:05 -0500

    update queries

---
+++ Projects/Phase_3/src/Updated to Tables.sql
@@ -0,0 +1,52 @@
+
+INSERT INTO CarModel (Year, Name, Make, ClassCar_Name)
+VALUES
+(2000,"GTI","Volkswagen","Compact"),
+(2001,"Veloster","Hyundai","Compact"),
+(2002,"Corolla","Toyota","Compact"),
+(2003,"Spark","Chevrolet","Compact"),
+(2004,"Huracan","Lamborghini","Luxury"),
+(2005,"AMG 63","Mercedes-Benz","Luxury"),
+(2006,"CT5","Cadillac","Luxury"),
+(2007,"XF5","Jaguar","Luxury"),
+(2008,"Sienmna","Toyota","Minivan"),
+(2009,"Carnival","KIA","Minivan"),
+(2010,"Pacifica","Chrysler","Minivan"),
+(2011,"F-150","Ford","Pickup"),
+(2012,"Gladiator","Jeep","Pickup"),
+(2013,"Silverado","Chevrolet","Pickup"),
+(2014,"300","Chrysler","Sedan"),
+(2015,"A6","Audi","Sedan"),
+(2016,"Malibu","Chevrolet","Sedan"),
+(2017,"Taurus","Ford","Sedan"),
+(2018,"Transit","Ford","Van"),
+(2019,"Sprinter","Mercedes-Benz","Van"),
+(2020,"ProMaster","RAM","Van");
+
+
+
+INSERT INTO Car (VIM, Color, ClassCar_Name, CarModel_carmodelId)
+VALUES
+("100a1","red","Compact",6),
+("100a2","orange","Compact",7),
+("100a3","yellow","Compact",8),
+("100a4","green","Compact",9),
+("100a5","ligth blue","Luxury",10),
+("100a6","blue","Luxury",11),
+("100a7","purple","Luxury",12),
+("100a8","red","Luxury",13),
+("100a9","orange","Minivan",14),
+("100a10","yellow","Minivan",15),
+("100a11","green","Minivan",16),
+("100a12","light blue","Pickup",17),
+("100a13","blue","Pickup",18),
+("100a14","purple","Pickup",19),
+("100a15","red","Sedan",20),
+("100a16","orange","Sedan",21),
+("100a17","yellow","Sedan",22),
+("100a18","green","Sedan",23),
+("100a19","light blue","Van",24),
+("100a20","blue","Van",25),
+("100a21","light blue","Van",26);
+
+
